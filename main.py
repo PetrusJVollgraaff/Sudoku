c46f75582352424f5ca5c343d4ac81b6ef17795a
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2024-02-09 16:27:08 +0200

    Update

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from board import Board
 from Settings import *
 pygame.font.init()
 
+def draw_text(win, text, color, top, left, font ):
+    label = font.render(text, 1, color)
+
+    win.blit(label, (top - (label.get_width()/2), left - (label.get_height()/2)) )
 def main(win):
     bo = Board()
     bo.build_grid()
@@ -33,7 +37,28 @@ def main(win):
 
         pygame.display.update()
 
+def StartMenu(win):
+    run = True
+    font = pygame.font.SysFont('comicsans', 20, bold=True)
+    top = WIDTH / 2
+    left = HEIGHT / 2
+
+    while run:
+        win.fill((0, 0, 0))
+        draw_text(win, "Press Any Key To Play", (255, 255, 255), top, left, font)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                main(win)
+
+    pygame.display.quit()
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sudoku Game")
-main(win)
+StartMenu(win)
